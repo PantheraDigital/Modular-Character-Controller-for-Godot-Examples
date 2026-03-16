23f8bd59c000e7bb4936fda9e75b451297b58b02
Panthera <101964317+PantheraDigital@users.noreply.github.com>
2026-03-16 13:12:22 -0700

    Add files via upload

--- Generic-Character-3D/scripts/dash_pickup_trigger.gd
+++ Generic-Character-3D/scripts/dash_pickup_trigger.gd
@@ -10,11 +10,11 @@ func _on_body_entered(body: Node3D) -> void:
 		node.name = &"Dash"
 		
 		if body.name == &"ExampleCharacter":
-			node.set_script(load("res://controller_examples/scripts/actions/example_character/action_dash_with_anim.gd"))
+			node.set_script(load("res://scripts/actions/example_character/action_dash_with_anim.gd"))
 			node._anim_tree = body.find_child("AnimationTree")
 			node._collider_anim_player = body.find_child("CollisionShape3D").find_child("AnimationPlayer")
 		else:
-			node.set_script(load("res://controller_examples/scripts/actions/simple_character/action_dash.gd"))
+			node.set_script(load("res://scripts/actions/simple_character/action_dash.gd"))
 			node._collision_shape_3d = body.find_child("CollisionShape3D")
 		node._movement_class = body.find_child("GroundedMovement")
 		
